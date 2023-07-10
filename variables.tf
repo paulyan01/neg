57aed9b530b771b2d349b37e8c45fc4a3a53831b
paulyan01 <126923309+paulyan01@users.noreply.github.com>
2023-07-10 19:55:12 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
-variable "project_id" {test-02-12345}
-variable "name"{neg01}
-variable "function_name" {SERVERLESS}
+variable "project_id" {}
+variable "name"{}
+variable "function_name" {}
 variable "region" {
 type = list(string) 
 default=["northamerica-northeast1","us-central1"]

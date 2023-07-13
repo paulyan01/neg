5c37ae47b9ba30f1923914c1969377ba83f0cd26
paulyan01 <126923309+paulyan01@users.noreply.github.com>
2023-07-12 20:56:16 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,3 +12,14 @@ resource "google_compute_network_endpoint_group" "neg_00" {
   default_port = "90"
   zone         = "northamerica-northeast1-a"
 }
+resource "google_compute_network" "default" {
+  name                    = "neg-network-01"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "default" {
+  name          = "neg-subnetwork"
+  ip_cidr_range = "10.0.0.0/16"
+  region        = "us-central1"
+  network       = google_compute_network.default.id
+}
